576c78d1e5a289ae46bfd476c361b69316bef253
Jurgen <jurgen.geitner@gmail.com>
2024-09-15 10:12:57 +0200

    extending eventEmitter

--- app.js
+++ app.js
@@ -1,34 +1,10 @@
 const EventEmitter = require('node:events');
 
-// create an instance of the class
-const emitter = new EventEmitter();
+const Logger = require('./logger');
+const logger = new Logger();
 
-//Register a listener
-/* emitter.on('messageLogged', function(){
-    console.log('Listener called');
-});
- */
-
-//Register a listener with event argument
-/* emitter.on('messageLogged', function(arg){ // sometimes arg is replaced by e, eventArg etc
-    console.log('Listener called ', arg);
-});
-*/
-
-//Register a listener with event argument. An now with an ES6 arrow function
-emitter.on('messageLogged', (arg) => { // sometimes arg is replaced by e, eventArg etc
+logger.on('messageLogged', (arg) => { 
     console.log('Listener called ', arg);
 });
 
-emitter.on('logging', (arg) => { // sometimes arg is replaced by e, eventArg etc
-    console.log('Listener called ', arg);
-});
-
-//Raise an event
-//emitter.emit('messageLogged');
-
-//Raise an event and encapsulate values about the event in an object (i.e. event argument);
-emitter.emit('messageLogged', {id:1, url:'http://something.good'});
-
-//Raise: logging (data: message)
-emitter.emit('logging', {data:'The clouds are puffy in Arkansas'});
+logger.log('message');
--- logger.js
+++ logger.js
@@ -1,13 +1,14 @@
-console.log(__filename);
-console.log(__dirname);
+const EventEmitter = require('node:events');
 
-
-
-var url = 'http://mylogger.io./log';
-
-function log(message) {
-    // Send an HTTP request
-    console.log(message);
+// When a function is inside a class, we call it a method
+class Logger extends EventEmitter{
+    log(message) {
+        // Send an HTTP request
+        console.log(message);
+        //Raise an event and encapsulate values about the event in an object (i.e. event argument);
+    this.emit('messageLogged', {id:1, url:'http://something.good'});
+    }
 }
 
-module.exports = log;
+
+module.exports = Logger;
