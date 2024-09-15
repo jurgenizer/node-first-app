be5fb8206dd3e8a74b8cab487be35028606f025d
Jurgen <jurgen.geitner@gmail.com>
2024-09-15 09:37:29 +0200

    event arguments

--- app.js
+++ app.js
@@ -4,10 +4,31 @@ const EventEmitter = require('node:events');
 const emitter = new EventEmitter();
 
 //Register a listener
-emitter.on('messageLogged', function(){
+/* emitter.on('messageLogged', function(){
     console.log('Listener called');
+});
+ */
 
+//Register a listener with event argument
+/* emitter.on('messageLogged', function(arg){ // sometimes arg is replaced by e, eventArg etc
+    console.log('Listener called ', arg);
+});
+*/
+
+//Register a listener with event argument. An now with an ES6 arrow function
+emitter.on('messageLogged', (arg) => { // sometimes arg is replaced by e, eventArg etc
+    console.log('Listener called ', arg);
+});
+
+emitter.on('logging', (arg) => { // sometimes arg is replaced by e, eventArg etc
+    console.log('Listener called ', arg);
 });
 
 //Raise an event
-emitter.emit('messageLogged');
+//emitter.emit('messageLogged');
+
+//Raise an event and encapsulate values about the event in an object (i.e. event argument);
+emitter.emit('messageLogged', {id:1, url:'http://something.good'});
+
+//Raise: logging (data: message)
+emitter.emit('logging', {data:'The clouds are puffy in Arkansas'});
