6e6712f240cf35cb10f3e091f51bdf63172e82d4
Jurgen <jurgen.geitner@gmail.com>
2024-09-15 09:21:49 +0200

    intro to events module

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const fs = require('node:fs');
+const EventEmitter = require('node:events');
 
-//const fs = require('node:fs/promises');
+// create an instance of the class
+const emitter = new EventEmitter();
 
-const files =  fs.readdir('./', function(err, files) {
-//check if we have an error or a result
-//only on of these argument s will have a value and the other will be null
-if (err) console.log('Error', err);
-else console.log('Result', files);
+//Register a listener
+emitter.on('messageLogged', function(){
+    console.log('Listener called');
 
 });
 
+//Raise an event
+emitter.emit('messageLogged');
